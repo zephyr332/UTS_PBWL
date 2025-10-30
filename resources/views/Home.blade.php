0f68a74a567137912b960a73242c129f3ef50138
zephyr332 <hafizhfahriza1508@gmail.com>
2025-10-30 19:54:41 +0700

    Membuat Navbar Home

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>UTS Laravel - Halaman Home</title>
+  <!-- Bootstrap 5 CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+  <!-- Navbar -->
+    <nav class="navbar bg-light shadow-sm">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+        <div class="d-flex align-items-center">
+        <a class="navbar-brand fw-semibold me-3" href="#">UTS Laravel</a>
+        <h1 class="fw-bold fs-3 mb-0" style="margin-top: 4px;">Halaman Home</h1>
+        </div>
+
+        <!-- Tombol Sidebar -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    </nav>
+
+
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
