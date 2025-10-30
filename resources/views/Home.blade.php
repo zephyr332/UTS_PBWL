93a331219bc33142d76fde2c31691c2cb7bf70f4
zephyr332 <hafizhfahriza1508@gmail.com>
2025-10-30 19:57:34 +0700

    Membuat Sidebar Home

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -10,21 +10,42 @@
 <body>
 
   <!-- Navbar -->
-    <nav class="navbar bg-light shadow-sm">
-    <div class="container-fluid d-flex justify-content-between align-items-center">
-        <div class="d-flex align-items-center">
-        <a class="navbar-brand fw-semibold me-3" href="#">UTS Laravel</a>
-        <h1 class="fw-bold fs-3 mb-0" style="margin-top: 4px;">Halaman Home</h1>
-        </div>
-
-        <!-- Tombol Sidebar -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-        aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
+<nav class="navbar bg-light shadow-sm">
+  <div class="container-fluid d-flex justify-content-between align-items-center">
+    <div class="d-flex align-items-center">
+      <a class="navbar-brand fw-semibold me-3" href="#">UTS Laravel</a>
+      <h1 class="fw-bold fs-3 mb-0" style="margin-top: 4px;">Halaman Home</h1>
     </div>
-    </nav>
 
+    <!-- Tombol Sidebar -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+      aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
+
+  <!-- Sidebar Offcanvas -->
+  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title fw-bold" id="offcanvasNavbarLabel">UTS Laravel</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Produk</a>
+        </li>
+      </ul>
+      <form class="d-flex mt-3" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
 
   <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
